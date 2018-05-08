8c08a89c2caaa4c9c72e1367de8aa9b04618de16
zyiz <1109580034@qq.com>
2018-05-08 19:39:09 +0800

    增加轮播图随tableview的下拉而拉伸，并增加了标题和左按钮

--- zhihu/zhihu/Home/Controller/HomeViewController.m
+++ zhihu/zhihu/Home/Controller/HomeViewController.m
@@ -24,6 +24,12 @@
 
 @property (nonatomic,strong)BannerView *bannerView;
 
+@property (nonatomic,strong)UIView *headerView;
+
+@property (nonatomic,strong)UILabel *titleLable;
+
+@property (nonatomic,strong)UIButton *leftBtn;
+
 @end
 
 @implementation HomeViewController
@@ -34,16 +40,60 @@
     self.view.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.tableView];
     [self.view addSubview:self.bannerView];
-    
-    self.bannerView.sd_layout.topSpaceToView(self.view, 0).leftSpaceToView(self.view, 0).rightSpaceToView(self.view, 0).heightIs(220);
+    [self.view addSubview:self.headerView];
+    //self.bannerView.sd_layout.topSpaceToView(self.view, 0).leftSpaceToView(self.view, 0).rightSpaceToView(self.view, 0).heightIs(220);
     
     [self getLates];
 }
 
-#pragma mark -- 懒加载
+#pragma mark -- 导航栏
+- (UIView *)headerView{
+    if (!_headerView) {
+        _headerView = [UIView new];
+        _headerView.backgroundColor = [UIColor colorWithRed:23/255.0 green:150/255.0 blue:210/255.0 alpha:0];
+        _headerView.frame = CGRectMake(0, 0, SCREEN_WIDTH, 60);
+        
+        [_headerView addSubview:self.leftBtn];
+        [_headerView addSubview:self.titleLable];
+        _leftBtn.sd_layout.topSpaceToView(_headerView, 25).leftSpaceToView(_headerView, 12).widthIs(25).heightIs(25);
+        _titleLable.sd_layout.centerXEqualToView(_headerView).topEqualToView(_leftBtn).heightIs(25);
+    }
+    return _headerView;
+}
+
+- (UILabel *)titleLable{
+    if (!_titleLable) {
+        _titleLable = [UILabel new];
+        _titleLable.attributedText = [[NSAttributedString alloc]initWithString:@"今日要闻" attributes:@{
+                                                                                                    NSFontAttributeName:[UIFont systemFontOfSize:20],
+                                                                                                    NSForegroundColorAttributeName:[UIColor whiteColor]
+                                                                                                    
+                                                                                                        }];
+        [_titleLable sizeToFit];
+        _titleLable.textAlignment = NSTextAlignmentCenter;
+    }
+    return _titleLable;
+}
+
+- (UIButton *)leftBtn{
+    if (!_leftBtn) {
+        _leftBtn = [UIButton new];
+        [_leftBtn addTarget:self action:@selector(didLeftMenuButton) forControlEvents:UIControlEventTouchUpInside];
+        [_leftBtn setImage:[UIImage imageNamed:@"icon_home_left"] forState:UIControlStateNormal];
+        
+    }
+   return _leftBtn;
+}
+
+-(void)didLeftMenuButton{
+    
+}
+
+#pragma mark -- 轮播图懒加载
 - (BannerView *)bannerView{
     if (!_bannerView) {
         _bannerView = [[BannerView alloc]init];
+        _bannerView.frame = CGRectMake(0, 0, self.view.frame.size.width, 240);
     }
     return _bannerView;
 }
@@ -95,6 +145,35 @@
     cell.model = self.newsArray[indexPath.row];
     return cell;
 }
+
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
+    if ([keyPath isEqualToString:@"contentOffset"]) {
+        CGFloat yOffset = self.tableView.contentOffset.y;
+        if (yOffset <= 0) {
+            self.bannerView.height = 200 - yOffset;
+            CGRect frame = self.bannerView.frame;
+            frame.origin.y = 0;
+            self.bannerView.frame = frame;
+        }else{
+            CGRect frame = self.bannerView.frame;
+            frame.origin.y = 0 - yOffset;
+            self.bannerView.frame = frame;
+        }
+        
+        CGFloat alpha = 0;
+        if (yOffset < 100) {
+            alpha = 0;
+        }else if (yOffset < 165){
+            alpha = (yOffset - 100) / (165 - 100);
+        }else{
+            alpha = 1;
+        }
+        _headerView.backgroundColor = [UIColor colorWithRed:23/255.0 green:150/255.0 blue:210/255.0 alpha:alpha];
+    }
+}
+
+
 #pragma mark -- 表视图懒加载
 -(UITableView*)tableView{
     if (!_tableView) {
@@ -103,6 +182,8 @@
         _tableView.delegate = self;
         [_tableView registerClass:[HomeTableViewCell class] forCellReuseIdentifier:CELLID];
         _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        
+        [_tableView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
         
         UIView *headView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 200)];
         _tableView.tableHeaderView = headView;
